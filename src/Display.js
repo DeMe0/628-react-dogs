70134cccd103bcaeb228aab6ff96ada88b340abf
Anthony DeMeo <anthonydemeo@anthonys-mbp.home>
2021-08-16 14:34:45 -0400

    deploying front end dogs api site

--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  const {dogs} = props
+  const loaded = () => (<div style={{textAlign: "center"}}>
+    {dogs.map((dog) => (
+      <article>
+        <img src={dog.img}/>
+        <h1>{dog.name}</h1>
+        <h3>{dog.age}</h3>
+        <button onClick={() => {
+          // setting the selectDog state
+          props.selectDog(dog)
+          // redirecting us to the edit form
+          props.history.push('/edit')
+        }}>Edit</button>
+        <button
+            onClick={() => {
+              props.deleteDog(dog);
+            }}
+          >
+            Delete
+          </button>
+      </article>
+    ))}
+  </div>)
+
+  const loading = <h1>Loading...</h1>
+
+  return dogs.length > 0 ? loaded() : loading
 };
 
 export default Display;
